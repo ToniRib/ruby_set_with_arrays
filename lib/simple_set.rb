41cebf3e3b5bcc50b40fcd755c26a90116cf40ce
ToniRib <tonimarierib@gmail.com>
2016-08-18 19:37:42 -0600

    Refactor and extract logical statements

--- lib/simple_set.rb
+++ lib/simple_set.rb
@@ -25,21 +25,33 @@ class SimpleSet
     @elements.concat(new_elements.uniq.compact).uniq!
   end
 
-  def ==(other_set)
-    size == other_set.size && elements.sort == other_set.elements.sort
+  def ==(other)
+    equal_size(other) && sorted_elements_match(other)
   end
 
   def subset?(other_set)
-    size <= other_set.size && other_set_contains_all_elements(other_set)
+    same_size_or_smaller(other_set) && contains_all_elements(other_set)
   end
 
   private
+
+  def equal_size(set)
+    size == set.size
+  end
+
+  def sorted_elements_match(set)
+    elements.sort == set.elements.sort
+  end
+
+  def same_size_or_smaller(set)
+    size <= set.size
+  end
 
   def element_is_nil_or_already_in_set(element)
     element.nil? || contains?(element)
   end
 
-  def other_set_contains_all_elements(set)
+  def contains_all_elements(set)
     elements.all? { |element| set.contains?(element) }
   end
 end
